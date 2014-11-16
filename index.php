2ffb0398159e70ad0284c4b44f14cc7526e8de9b
maardz <moldermarge@gmail.com>
2014-11-16 23:24:20 +0200

    Final commit

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
   <head>
-
+ 	<title>Praktikum 3</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   </head>
   <body>
-
+  	<form action="form_processing.php" method="post">
+		<span>Kasutaja: </span>		
+		<input type="text" name="username"><br>
+		
+		<span>Parool: </span>
+		<input type="password" name="password"><br>
+		
+		<input type="submit" name="submit"><br>
+	</form>
   </body>
 </html>
